49a9e62c2d3b891f1affa404f9fd4130e5833f4c
thiagoribb <thiribeiro142@gmail.com>
2021-06-25 17:33:14 -0300

    Preparando review

--- src/utils/DateFormatter.js
+++
@@ -1,8 +0,0 @@
-const DateFormatter = date => {
-    const formattedDate = date.getFullYear() + '-' +
-        ('0' + (date.getMonth() + 1)).slice(-2) + '-' +
-        ('0' + date.getDate()).slice(-2);
-    return formattedDate;
-}
-
-export default DateFormatter;
